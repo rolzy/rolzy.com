198f38590688985219256a4eb951b376517b7a29
Roland Thompson <rolanditaru@gmail.com>
2021-07-16 09:11:22 +1000

    Change layout a little

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "Rolzyland",
   },
   plugins: [
     "gatsby-plugin-image",
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hello There!</p>
-      <p>General Kenobi! You are a bold one. Kill him!</p>
+      <p>That is me with a deer in Nara, Japan.</p>
+      <StaticImage
+        alt="Clifford"
+        src="../images/1.PNG"
+      />
     </Layout>
   )
 }
