24be0298d63d89ff996de9cdacd813b175ad856f
Roland Thompson <rolanditaru@gmail.com>
2021-07-15 11:10:47 +1000

    Use for loops to render board

--- src/index.js
+++ src/index.js
@@ -24,23 +24,17 @@ class Board extends React.Component {
   }
 
   render() {
+    const board = [];
+    for (let i=0; i < 3; i++) {
+      let squares = [];
+      for (let j=0; j < 3; j++) {
+        squares.push(this.renderSquare(3*i+j))
+      };
+      board.push(<div className="board-row">{squares}</div>);
+    };
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div> 
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div> 
+        {board}
       </div>
     );
   }
