b1d853f9469c2cc7a8f1f84b8cd3298101882265
Roland Thompson <rolanditaru@gmail.com>
2021-07-15 16:47:35 +1000

    Add gatsby code

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hello There!</p>
+      <p>General Kenobi! You are a bold one. Kill him!</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
