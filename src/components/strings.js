f51806d157044450b394ea299bd39cbd54849cf3
Roland Thompson <rolanditaru@gmail.com>
2021-08-01 11:43:25 +1000

    Language swap working for about page

--- src/components/strings.js
+++ src/components/strings.js
@@ -6,7 +6,10 @@ export const strings = {
     homepage: 'Home Page',
     greetings: '<p>My name is Roland Thompson.</p> \
     <p>I put up tips and tricks that I have gathered over the years on topics about coding and computers.</p> \
-    <p>I will put up links to my projects that you can try out too!</p>'
+    <p>I will put up links to my projects that you can try out too!</p>',
+    aboutpage: 'About Me',
+    aboutme: '<p>Hello There!</p> \
+    <p>That is me with a deer in Nara, Japan.</p>',
   },
   ja: {
     changeLang: 'English',
@@ -16,5 +19,8 @@ export const strings = {
     greetings: '<p>こんにちは！</p><p>トンプソンといいます。</p> \
     <p>ここでは、ITやdevに関する記事をたまに共有してます。</p> \
     <p>また、これまで作ったものをブラウザ上で気軽に試せる環境も用意するので、お楽しみに！</p>',
+    aboutpage: 'プロフィール',
+    aboutme: '<p>こんにちは！</p> \
+    <p>写真の右側が私です。</p>',
   },
 };
